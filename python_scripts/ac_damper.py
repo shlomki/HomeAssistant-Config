8a1cfb937d2eea2fa50a6169301417466a0a295a
shlomki <shlomki@gmail.com>
2020-10-21 09:49:32 +0300

    Fix AC Quick, Refactor ac_damper.py, remove some deprecations

--- python_scripts/ac_damper.py
+++ python_scripts/ac_damper.py
@@ -17,13 +17,22 @@ last_ac_mode_entity_id = "input_text.last_ac_mode"
 
 #Vars
 damper_state = hass.states.get(damper_state_entity_id).state
+last_ac_mode = hass.states.get(last_ac_mode_entity_id).state
+
+def send_command(room, speed):
+  command_entity_id = "ac_damper_" + speed.lower()
+  if (speed != "off"):
+    command_entity_id = command_entity_id + "_" + last_ac_mode
+
+  if enableLog:
+    logger.debug("Run script: script." + command_entity_id + " in room: " + room)
+  hass.services.call('script', command_entity_id , { 'room' : room } , options)
 
 def turn_damper_on():
   hass.services.call('input_boolean', "turn_on" , { 'entity_id': damper_state_entity_id, }, options)
   if enableLog:
     logger.debug("Turn on entity_id: " + damper_state_entity_id)
 
-  last_ac_mode = hass.states.get(last_ac_mode_entity_id).state
   speed = hass.states.get(damper_speed_entity_id).state
 
   if (speed == 'SmartAuto'): 
@@ -31,11 +40,7 @@ def turn_damper_on():
   elif (speed == 'Quick'):
     speed = "high"
 
-  command_entity_id = "ac_damper_" + speed.lower() + "_" + last_ac_mode
-  
-  if enableLog:
-    logger.debug("Run script: script." + command_entity_id + " in room: " + room)
-  hass.services.call('script', command_entity_id , { 'room' : room } , options)
+  send_command(room, speed)
 
 if (action == 'set_speed'):
   #Just save the new speed
@@ -46,6 +51,9 @@ if (action == 'set_speed'):
   if (damper_state == 'on'):
     turn_damper_on()
 
+if (action == 'send_command'):
+  send_command(room, requested_speed)
+  
 if (action == 'turn_on') and (requested_speed == 'None'): #This is done to ignore turning on the damper when changing speed when it's off
   turn_damper_on()
 
@@ -54,23 +62,18 @@ if (action == 'turn_off') :
     logger.debug("Turn off entity_id: " + damper_state_entity_id)
   hass.services.call('input_boolean', "turn_off" , { 'entity_id': damper_state_entity_id }, options)
   
-  if enableLog:
-    logger.debug("Run script: script.ac_damper_off" + " in room: " + room)
-  hass.services.call('script', 'ac_damper_off' , { 'room' : room } , options)
+  send_command(room, "off")
 
 if (action == 'sync') :
   hass.services.call('input_boolean', "turn_on" , { 'entity_id': 'input_boolean.ac_damper_oscillating' }, options)
   if (damper_state == 'on') :
     last_ac_mode = hass.states.get(last_ac_mode_entity_id).state
     damper_speed = hass.states.get(damper_speed_entity_id).state
-    command_entity_id = "ac_damper_" + damper_speed.lower() + "_" + last_ac_mode
+    send_command(room, damper_speed)
 
   else :
-    command_entity_id = "ac_damper_off"
+    send_command(room, "off")
   
-  if enableLog:
-    logger.debug("Run script: script." + command_entity_id + " in room: " + room)
-  hass.services.call('script', command_entity_id , { 'room' : room } , options)
   hass.services.call('input_boolean', "turn_off" , { 'entity_id': 'input_boolean.ac_damper_oscillating' }, options)
 
   #Update current temp
