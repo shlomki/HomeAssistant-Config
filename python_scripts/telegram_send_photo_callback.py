0631d120e8f03229aa6a6549ef455e45c777c630
shlomki <shlomki@gmail.com>
2021-07-23 11:14:18 +0300

    Door opened action buttons in telegram

--- python_scripts/telegram_send_photo_callback.py
+++ python_scripts/telegram_send_photo_callback.py
@@ -4,15 +4,13 @@ options = { 'blocking': True }
 callback_query_id = data['callback_query_id']
 callback_data = data['callback_data']
 callback_data = callback_data.split(' ')
-action = callback_data[0].replace('/', '')
-entity_id = callback_data[1]
+chat_id = data['chat_id']
+file = callback_data[1]
 
 #Turn on / turn off
-hass.services.call('homeassistant', action, { 'entity_id': entity_id }, options)
+hass.services.call('script', 'notify_photo', { 'file': file, 'target': chat_id, 'message': 'Photo' }, options)
 
 #Send callback that confirms new state
-entity_data = hass.states.get(entity_id)
-new_state = entity_data.state
-entity_name = entity_data.attributes['friendly_name']
-message = 'OK, ' + entity_name + ' is now ' + new_state
+message = 'OK, photo sent.'
 hass.services.call('telegram_bot', 'answer_callback_query', { 'callback_query_id': callback_query_id, 'message': message }, options)
+
