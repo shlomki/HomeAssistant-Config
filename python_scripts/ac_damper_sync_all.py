730c3747c09fb7b9deb05c6227c64b12f2ce46d6
shlomki <shlomki@gmail.com>
2022-10-11 22:45:34 +0300

    Rename Birds to Guests

--- python_scripts/ac_damper_sync_all.py
+++ python_scripts/ac_damper_sync_all.py
@@ -1,5 +1,5 @@
 
 options = { 'blocking': True }
-rooms = ["living_room", "office", "birds", "riley", "bedroom"]
+rooms = ["living_room", "office", "guests", "riley", "bedroom"]
 for room in rooms:
   hass.services.call('python_script', "ac_damper" , { 'room': room, 'action': 'sync' }, options)
