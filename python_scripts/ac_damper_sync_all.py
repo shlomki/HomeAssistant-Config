e9004d913056aede55e54c391e6ecbb63ebaaba6
shlomki <shlomki@gmail.com>
2023-06-17 18:18:28 +0000

    Delete old scripts

--- python_scripts/ac_damper_sync_all.py
+++
@@ -1,5 +0,0 @@
-
-options = { 'blocking': True }
-rooms = ["living_room", "office", "guests", "riley", "bedroom"]
-for room in rooms:
-  hass.services.call('python_script', "ac_damper" , { 'room': room, 'action': 'sync' }, options)
--- python_scripts/loop.py
+++
@@ -1,5 +0,0 @@
-service_data = { 'room': data['room'] }
-options = { 'blocking': True }
-
-for i in range(int(data['loop'])):
-  hass.services.call(data['domain'], data['entity_id'], service_data, options)
